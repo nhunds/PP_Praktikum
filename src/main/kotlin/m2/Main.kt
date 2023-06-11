bf43e23cef4a2292e96a7012fada2e7d47db3222
Daniel Nico Hunds <daniel_nico.hunds@smail.th-koeln.de>
2023-06-11 15:43:55 +0200

    rework to use currying and extension function syntax

--- src/main/kotlin/m2/Main.kt
+++ src/main/kotlin/m2/Main.kt
@@ -1,47 +1,61 @@
 package m2
 
-typealias Ordering<A> = (A, A) -> OrderResult;
+typealias Ordering<A> = (A) -> (A) -> OrderResult;
 
-val intOrd: Ordering<Int> = { left, right ->
-    if (left < right) OrderResult.Lower
-    else if (left > right) OrderResult.Higher
-    else OrderResult.Equal
-}
-
-val stringOrd: Ordering<String> = { left, right ->
-    if (left < right) OrderResult.Lower
-    else if (left > right) OrderResult.Higher
-    else OrderResult.Equal
-}
-
-val doubleOrd: Ordering<Double> = { left, right ->
-    if (left < right) OrderResult.Lower
-    else if (left > right) OrderResult.Higher
-    else OrderResult.Equal
-}
-
-val booleanOrd: Ordering<Boolean> = { left, right ->
-    if (left < right) OrderResult.Lower
-    else if (left > right) OrderResult.Higher
-    else OrderResult.Equal
-}
-
-fun<A> reversed(ordering: Ordering<A>): Ordering<A> = { left, right ->
-    when (ordering(left, right)) {
-        OrderResult.Lower -> OrderResult.Higher
-        OrderResult.Higher -> OrderResult.Lower
-        else -> OrderResult.Equal
+val intOrd: Ordering<Int> = { left ->
+    { right ->
+        if (left < right) OrderResult.Lower
+        else if (left > right) OrderResult.Higher
+        else OrderResult.Equal
     }
 }
 
-fun<A> debug(ordering: Ordering<A>): Ordering<A> = { left, right ->
-    val orderResult = ordering(left, right)
-    println("$left $orderResult $right")
-    orderResult
+val stringOrd: Ordering<String> = { left ->
+    { right ->
+        if (left < right) OrderResult.Lower
+        else if (left > right) OrderResult.Higher
+        else OrderResult.Equal
+    }
 }
 
-fun<A, B> Ordering<A>.contraMap(transform: (B) -> A): Ordering<B> = { left, right ->
-    this(transform(left), transform(right))
+val doubleOrd: Ordering<Double> = { left ->
+    { right ->
+        if (left < right) OrderResult.Lower
+        else if (left > right) OrderResult.Higher
+        else OrderResult.Equal
+    }
+}
+
+val booleanOrd: Ordering<Boolean> = { left ->
+    { right ->
+        if (left < right) OrderResult.Lower
+        else if (left > right) OrderResult.Higher
+        else OrderResult.Equal
+    }
+}
+
+fun<A> Ordering<A>.reversed(): Ordering<A> = { left ->
+    { right ->
+        when (this(left)(right)) {
+            OrderResult.Lower -> OrderResult.Higher
+            OrderResult.Higher -> OrderResult.Lower
+            else -> OrderResult.Equal
+        }
+    }
+}
+
+fun<A> Ordering<A>.debug(): Ordering<A> = { left ->
+    { right ->
+        val orderResult = this(left)(right)
+        println("$left $orderResult $right")
+        orderResult
+    }
+}
+
+fun<A, B> Ordering<A>.contraMap(transform: (B) -> A): Ordering<B> = { left ->
+    { right ->
+        this(transform(left))(transform(right))
+    }
 }
 
 val personNameOrd = stringOrd.contraMap { person: Person ->
@@ -53,13 +67,13 @@ val personAgeOrd = intOrd.contraMap { person: Person ->
 }
 
 fun main() {
-    val intDesc = reversed(intOrd)
-    val string = debug(stringOrd)
-    string("hallo", "weggehen")
-    val doubleDesc = debug(reversed(doubleOrd))
-    doubleDesc(0.5, 1.5)
+    val intDesc = intOrd.reversed()
+    val string = stringOrd.reversed()
+    string("hallo")("weggehen")
+    val doubleDesc = doubleOrd.reversed().debug()
+    doubleDesc(0.5)(1.5)
     val person1 = Person("Max Muster", 40)
     val person2 = Person("Melanie Müller", 30)
-    debug(personNameOrd)(person1, person2)
-    debug(personAgeOrd)(person1, person2)
+    personNameOrd.debug()(person1)(person2)
+    personAgeOrd.debug()(person1)(person2)
 }
