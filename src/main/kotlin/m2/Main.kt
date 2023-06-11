dbba7bb489ea9f833dc426d3e9d75d4254fc07ae
Daniel Nico Hunds <daniel_nico.hunds@smail.th-koeln.de>
2023-06-11 11:44:20 +0200

    implemented intOrd

--- src/main/kotlin/m2/Main.kt
+++ src/main/kotlin/m2/Main.kt
@@ -1,3 +1,12 @@
 package m2
 
 typealias Ordering<A> = (A, A) -> OrderResult;
+
+val intOrd: Ordering<Int> = { left, right ->
+    if (left < right) OrderResult.Lower
+    if (left > right) OrderResult.Higher
+    OrderResult.Equal
+}
+fun main() {
+
+}
